49a40a224c38cae26996f6e52e9f366057de4038
jhorn26 <jeanfh26@gmail.com>
2023-12-08 13:22:49 -0300

    Função para ordenar os endereços

---
+++ sort.h
@@ -0,0 +1,18 @@
+#ifndef SORT_H
+#define SORT_H
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+bool operator>(const std::tuple<int, int, float, int>& a, const std::tuple<int, int, float, int>& b);
+
+void swap(std::vector<std::tuple<int, int, float, int>>* address, int i, int j);
+
+void heapify(std::vector<std::tuple<int, int, float, int>>* address, int n, int root);
+
+void buildHeap(std::vector<std::tuple<int, int, float, int>>* address, int n);
+
+void heapSort(std::vector<std::tuple<int, int, float, int>>* address);
+
+
+#endif

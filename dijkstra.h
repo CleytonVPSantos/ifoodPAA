a645838902ceacf5e0933c6b2d223ccf518924ac
CleytonVPSantos <cleyton.santos@fgv.edu.br>
2023-12-08 16:31:28 -0300

    resolve erro dijkstra header

--- dijkstra.h
+++ dijkstra.h
@@ -4,12 +4,24 @@
 #include <tuple>
 #include "graph.h"
 
-bool compare(std::pair<double, int> a, std::pair<double, int> b);
+bool compare(std::pair<double, int>, std::pair<double, int> );
 
 std::vector<int> findNClosest(Vertex, Graph, int);
 
+std::vector<std::vector<int>> pathsOfNClosest(Vertex, Graph, int);
+
 std::vector<int> dijkstraWithGoal(Vertex, Vertex, Graph);
+
+std::vector<int> mergePaths(std::vector<int>, std::vector<int>);
+
+std::vector<int> simpleDelivery(Vertex, Vertex, Vertex, Graph);
 
 std::vector<int> buildPath(Vertex, Graph, int*);
 
 std::vector<int> mergePaths(std::vector<int>, std::vector<int>);
+
+std::vector<std::tuple<int, int, std::vector<int>>> optmizedDelivery(Vertex, Graph, int);
+
+std::vector<std::pair<std::vector<int>, float>> pathToEachCenter(Vertex, Graph);
+
+int countDistributionCenters(Graph);

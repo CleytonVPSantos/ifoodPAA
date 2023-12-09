766f65bf0fd1cd666fa64ee4ff0adf6dbdd37a8d
Otávio Augusto <otavio.oam@protonmail.com>
2023-12-09 09:24:41 -0300

    corrija bugs em graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -3,36 +3,14 @@
 void Graph::deleteTemporalVertices() {
     // Iterate through each vertex in the graph
     for(int i = 0; i < numVertices; i++){
-        // Get the first edge node of the current vertex
-        std::vector<Node> adjList = edges[i];
-
-        // Traverse the linked list of edges for the current vertex
-        for(unsigned j = 0; j < adjList.size(); j++){
+        // Iterate through each edge of the current vertex
+        for(unsigned int j = 0; j < edges[i].size(); j++){
             // Check if the connected vertex is not of type 0 (not temporal)
-            if(vertices[adjList[j].vertexId].type != 0){
-                // If so, delete the edgeNode and update the linked list
-                adjList.erase(adjList.begin()+j);
-                // If the edgeNode is the first in the list, update edges[i]
-                // if(edgeNode == edges[i]){
-                //     edges[i] = edgeNode->next;
-                // }
-                // // If the edgeNode is not the first, update the linked list
-                // else{
-                //     Node* edgeNode2 = edges[i];
-                //     while(edgeNode2->next != edgeNode){
-                //         edgeNode2 = edgeNode2->next;
-                //     }
-                //     edgeNode2->next = edgeNode->next;
-                // }
-                // // Update tempNode, delete the current edgeNode, and move to the next
-                // tempNode = edgeNode->next;
-                // delete edgeNode;
-                // edgeNode = tempNode;
+            if(vertices[edges[i][j].vertexId].type != 0){
+                // If so, delete the edgeNode and update the vector of edges
+                edges[i].erase(edges[i].begin()+j);
+                j--;
             }
-            // If the connected vertex is of type 0, move to the next edgeNode
-            // else{
-            //     edgeNode = edgeNode->next;
-            // }
         }
     }
 
@@ -52,36 +30,9 @@ void Graph::addEdge(Vertex from, Vertex to, double weight) {
     int id2 = to.id;
 
     edges[id1].push_back(Node{id2, weight}); // add the new node to the end of the list
-    edges[id2].push_back(Node{id2, weight}); // add the new node to the end of the list
-    
-    // Add an edge from 'from' to 'to'
-    // Node* edgeNode = edges[id1];
-    // if(edgeNode == NULL){
-    //     // If there are no existing edges, create a new edgeNode
-    //     edges[id1] = new Node{id2, weight, NULL};
-    // }
-    // else{
-    //     // Traverse the linked list to the end and add a new edgeNode
-    //     while(edgeNode->next != NULL){
-    //         edgeNode = edgeNode->next;
-    //     }
-    //     edgeNode->next = new Node{id2, weight, NULL};
-    // }
-
-    // // Add an edge from 'to' to 'from' (since it's an undirected graph)
-    // Node* edgeNode2 = edges[id2];
-    // if(edgeNode2 == NULL){
-    //     // If there are no existing edges, create a new edgeNode
-    //     edges[id2] = new Node{id1, weight, NULL};
-    // }
-    // else{
-    //     // Traverse the linked list to the end and add a new edgeNode
-    //     while(edgeNode2->next != NULL){
-    //         edgeNode2 = edgeNode2->next;
-    //     }
-    //     edgeNode2->next = new Node{id1, weight, NULL};
-    // }
+    edges[id2].push_back(Node{id1, weight}); // add the new node to the end of the list
 }
+
 void Graph::addVertex(int id, int type) {
     // Create a new vertex with the given ID and type
     Vertex newVertex = {id, type};
@@ -114,24 +65,8 @@ double Graph::findEdgeWeight(int idVertex1, int idVertex2){
     }
 
     return weight;
-
-    // // Traverse the linked list of edges for the first vertex
-    // Node* edgeNode = edges[idVertex1];
-    // while(edgeNode != NULL){
-    //     // Check if the current edgeNode corresponds to the second vertex
-    //     if(edgeNode->vertexId == idVertex2){
-    //         // If found, update the weight and break out of the loop
-    //         weight = edgeNode->weight;
-    //         break;
-    //     }
-
-    //     // Move to the next edgeNode in the linked list
-    //     edgeNode = edgeNode->next;
-    // }
-
-    // // Return the weight of the edge (default value if the edge is not found)
-    // return weight;
 }
+
 void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* address) {
     // Sort the matrix by some criteria (not provided in the code)
     sortAddressList(address);
@@ -143,7 +78,7 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
     bool isSameEdge = true;
     unsigned int i = 0;
 
-    // Determine the number of vertices in each edge group
+    // Determine the beginning and end of each edge group
     while (i < (*address).size()) {
         isSameEdge = true;
         while (isSameEdge && numVerticesSameEdge < (*address).size()) {
@@ -157,31 +92,31 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
         groups.push_back(numVerticesSameEdge);
     }
 
-    double weight = 0;
     // Process each edge group and add vertices and edges accordingly
     for (unsigned int i = 0; i < groups.size() - 1; i++) {
         int j = groups[i];
-        weight = findEdgeWeight(std::get<0>((*address)[j]), std::get<1>((*address)[j]));
+        double weight = findEdgeWeight(std::get<0>((*address)[j]), std::get<1>((*address)[j]));
 
         // Add a new vertex and edge based on the current edge group
         addVertex(numVertices, std::get<3>((*address)[j]));
         addEdge(vertices[std::get<0>((*address)[j])], vertices[numVertices - 1], weight * std::get<2>((*address)[j]));
 
         // Depending on the number of vertices in the edge group, add additional vertices and edges
-        if (groups[i + 1] - groups[i] < 2) {
+        int groupSize = groups[i + 1] - groups[i];
+        if (groupSize == 1) {
             addEdge(vertices[numVertices - 1], vertices[std::get<1>((*address)[j])], weight * (1 - std::get<2>((*address)[j])));
-        } else if (groups[i + 1] - groups[i] < 3) {
+        } else if (groupSize == 2) {
             addVertex(numVertices, std::get<3>((*address)[j + 1]));
             addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + 1]) - std::get<2>((*address)[j])));
             addEdge(vertices[std::get<1>((*address)[j + 1])], vertices[numVertices - 1], weight * (1 - std::get<2>((*address)[j + 1])));
         } else {
-            for (int k = 1; k < groups[i + 1] - groups[i] - 1; k++) {
+            for (int k = 1; k < groupSize - 1; k++) {
                 addVertex(numVertices, std::get<3>((*address)[j + k]));
                 addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + k]) - std::get<2>((*address)[j + k - 1])));
             }
-            addVertex(numVertices, std::get<3>((*address)[j + groups[i + 1] - groups[i] - 1]));
-            addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + groups[i + 1] - groups[i] - 1]) - std::get<2>((*address)[j + groups[i + 1] - groups[i] - 2])));
-            addEdge(vertices[numVertices - 1], vertices[std::get<1>((*address)[j + groups[i + 1] - groups[i] - 1])], weight * (1 - std::get<2>((*address)[j + groups[i + 1] - groups[i] - 1])));
+            addVertex(numVertices, std::get<3>((*address)[j + groupSize - 1]));
+            addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + groupSize - 1]) - std::get<2>((*address)[j + groupSize - 2])));
+            addEdge(vertices[numVertices - 1], vertices[std::get<1>((*address)[j + groupSize - 1])], weight * (1 - std::get<2>((*address)[j + groupSize - 1])));
         }
     }
 }

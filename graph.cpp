89cb0d221df184bfef83d9a9ebe59226bd6054dc
Otávio Augusto <otavio.oam@protonmail.com>
2023-12-09 12:37:23 -0300

    corrija bugs em graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,15 +1,23 @@
 #include "graph.h"
 
+using namespace std;
+
 void Graph::deleteTemporalVertices() {
     // Iterate through each vertex in the graph
     for(int i = 0; i < numVertices; i++){
         // Iterate through each edge of the current vertex
-        for(unsigned int j = 0; j < edges[i].size(); j++){
-            // Check if the connected vertex is not of type 0 (not temporal)
-            if(vertices[edges[i][j].vertexId].type != 0){
-                // If so, delete the edgeNode and update the vector of edges
-                edges[i].erase(edges[i].begin()+j);
-                j--;
+        if(vertices[i].type != 0){
+            edges.erase(edges.begin()+i);
+        }
+        else{
+            for(unsigned int j = edges[i].size()-1; j >= 0; j--){
+                // Check if the connected vertex is not of type 0 (not temporal)
+                if(vertices[edges[i][j].vertexId].type != 0){
+                    // If so, delete the edgeNode and update the vector of edges
+                    edges[i].erase(edges[i].begin()+j);
+                }
+                else
+                    break;
             }
         }
     }
@@ -29,8 +37,8 @@ void Graph::addEdge(Vertex from, Vertex to, double weight) {
     int id1 = from.id;
     int id2 = to.id;
 
-    edges[id1].push_back(Node{id2, weight}); // add the new node to the end of the list
-    edges[id2].push_back(Node{id1, weight}); // add the new node to the end of the list
+    edges[id1].push_back(Node{id2, weight}); // add the node "to" to the end of the list
+    edges[id2].push_back(Node{id1, weight}); // add the node "from" to the end of the list
 }
 
 void Graph::addVertex(int id, int type) {
@@ -41,7 +49,7 @@ void Graph::addVertex(int id, int type) {
     vertices.push_back(newVertex);
 
     // Add empty vector of edges for the new vertex
-    edges.push_back(std::vector<Node>());
+    edges.push_back(vector<Node>());
     
     // Increment the total number of vertices
     numVertices++;
@@ -56,7 +64,7 @@ double Graph::findEdgeWeight(int idVertex1, int idVertex2){
     // Initialize weight to a default value (it will remain unchanged if the edge is not found)
     double weight = 0.0;
 
-    std::vector<Node> adjList = edges[idVertex1];
+    vector<Node> adjList = edges[idVertex1];
     for(unsigned int i = 0; i < adjList.size(); i++){
         if(adjList[i].vertexId == idVertex2){
             weight = adjList[i].weight;
@@ -67,12 +75,12 @@ double Graph::findEdgeWeight(int idVertex1, int idVertex2){
     return weight;
 }
 
-void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* address) {
+void Graph::addTemporalVertices(vector<tuple<int, int, double, int>>* address) {
     // Sort the matrix by some criteria (not provided in the code)
     sortAddressList(address);
 
     // Vector to store the number of vertices in the same edge group
-    std::vector<int> groups;
+    vector<int> groups;
     groups.push_back(0);
     unsigned int numVerticesSameEdge = 0;
     bool isSameEdge = true;
@@ -82,8 +90,8 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
     while (i < (*address).size()) {
         isSameEdge = true;
         while (isSameEdge && numVerticesSameEdge < (*address).size()) {
-            if (std::get<0>((*address)[i]) != std::get<0>((*address)[i + 1]) ||
-                std::get<1>((*address)[i]) != std::get<1>((*address)[i + 1])) {
+            if (get<0>((*address)[i]) != get<0>((*address)[i + 1]) ||
+                get<1>((*address)[i]) != get<1>((*address)[i + 1])) {
                 isSameEdge = false;
             }
             numVerticesSameEdge++;
@@ -95,28 +103,28 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
     // Process each edge group and add vertices and edges accordingly
     for (unsigned int i = 0; i < groups.size() - 1; i++) {
         int j = groups[i];
-        double weight = findEdgeWeight(std::get<0>((*address)[j]), std::get<1>((*address)[j]));
+        double weight = findEdgeWeight(get<0>((*address)[j]), get<1>((*address)[j]));
 
-        // Add a new vertex and edge based on the current edge group
-        addVertex(numVertices, std::get<3>((*address)[j]));
-        addEdge(vertices[std::get<0>((*address)[j])], vertices[numVertices - 1], weight * std::get<2>((*address)[j]));
+        // Add a new vertex and edge for the first vertex in the edge group
+        addVertex(numVertices, get<3>((*address)[j]));
+        addEdge(vertices[get<0>((*address)[j])], vertices[numVertices - 1], weight * get<2>((*address)[j]));
 
         // Depending on the number of vertices in the edge group, add additional vertices and edges
         int groupSize = groups[i + 1] - groups[i];
         if (groupSize == 1) {
-            addEdge(vertices[numVertices - 1], vertices[std::get<1>((*address)[j])], weight * (1 - std::get<2>((*address)[j])));
+            addEdge(vertices[numVertices - 1], vertices[get<1>((*address)[j])], weight * (1 - get<2>((*address)[j])));
         } else if (groupSize == 2) {
-            addVertex(numVertices, std::get<3>((*address)[j + 1]));
-            addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + 1]) - std::get<2>((*address)[j])));
-            addEdge(vertices[std::get<1>((*address)[j + 1])], vertices[numVertices - 1], weight * (1 - std::get<2>((*address)[j + 1])));
+            addVertex(numVertices, get<3>((*address)[j + 1]));
+            addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (get<2>((*address)[j + 1]) - get<2>((*address)[j])));
+            addEdge(vertices[get<1>((*address)[j + 1])], vertices[numVertices - 1], weight * (1 - get<2>((*address)[j + 1])));
         } else {
             for (int k = 1; k < groupSize - 1; k++) {
-                addVertex(numVertices, std::get<3>((*address)[j + k]));
-                addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + k]) - std::get<2>((*address)[j + k - 1])));
+                addVertex(numVertices, get<3>((*address)[j + k]));
+                addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (get<2>((*address)[j + k]) - get<2>((*address)[j + k - 1])));
             }
-            addVertex(numVertices, std::get<3>((*address)[j + groupSize - 1]));
-            addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (std::get<2>((*address)[j + groupSize - 1]) - std::get<2>((*address)[j + groupSize - 2])));
-            addEdge(vertices[numVertices - 1], vertices[std::get<1>((*address)[j + groupSize - 1])], weight * (1 - std::get<2>((*address)[j + groupSize - 1])));
+            addVertex(numVertices, get<3>((*address)[j + groupSize - 1]));
+            addEdge(vertices[numVertices - 1], vertices[numVertices - 2], weight * (get<2>((*address)[j + groupSize - 1]) - get<2>((*address)[j + groupSize - 2])));
+            addEdge(vertices[numVertices - 1], vertices[get<1>((*address)[j + groupSize - 1])], weight * (1 - get<2>((*address)[j + groupSize - 1])));
         }
     }
 }
@@ -135,7 +143,7 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
 //     myGraph.addEdge(myGraph.vertices[1], myGraph.vertices[2], 5);
 //     myGraph.addEdge(myGraph.vertices[0], myGraph.vertices[2], 2);
 
-//     std::cout << "\nGraph:" << std::endl;
+//     cout << "\nGraph:" << endl;
 //     for (int i = 0; i < myGraph.numVertices; i++) {
 //         for(int j = 0; j < myGraph.edges[i].size(); j++){
 //             // check if edge was already printed
@@ -146,22 +154,22 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
 //                 }
 //             }
 //             if(!alreadyPrinted)
-//                 std::cout << i << " --(" << myGraph.edges[i][j].weight << ")-- " << myGraph.edges[i][j].vertexId << std::endl;
+//                 cout << i << " --(" << myGraph.edges[i][j].weight << ")-- " << myGraph.edges[i][j].vertexId << endl;
 //         }
 //     }
 //     // for (int i = 0; i < myGraph.numVertices; i++) {
 //     //     Node* edgeNode = myGraph.edges[i];
 //     //     while(edgeNode != NULL){
-//     //         std::cout << i << " --(" << edgeNode->weight << ")-- " << edgeNode->vertexId << std::endl;
+//     //         cout << i << " --(" << edgeNode->weight << ")-- " << edgeNode->vertexId << endl;
 //     //         edgeNode = edgeNode->next;
 //     //     }
 //     // }
 
-//     std::vector<std::tuple<int, int, double, int>> address = {{0, 1, 0.1, 1}, {0,1,0.4,1}, {0,1,0.8,1}, {2,1,0.2,1}};
+//     vector<tuple<int, int, double, int>> address = {{0, 1, 0.1, 1}, {0,1,0.4,1}, {0,1,0.8,1}, {2,1,0.2,1}};
 //     myGraph.addTemporalVertices(&address);
     
 
-//     std::cout << "\nGraph with tempo:" << std::endl;
+//     cout << "\nGraph with tempo:" << endl;
 //     for (int i = 0; i < myGraph.numVertices; i++) {
 //         for(int j = 0; j < myGraph.edges[i].size(); j++){
 //             // check if edge was already printed
@@ -172,13 +180,13 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
 //                 }
 //             }
 //             if(!alreadyPrinted)
-//                 std::cout << i << " --(" << myGraph.edges[i][j].weight << ")-- " << myGraph.edges[i][j].vertexId << std::endl;
+//                 cout << i << " --(" << myGraph.edges[i][j].weight << ")-- " << myGraph.edges[i][j].vertexId << endl;
 //         }
 //     }
 //     myGraph.deleteTemporalVertices();
 
 
-//     std::cout << "\nGraph After:" << std::endl;
+//     cout << "\nGraph After:" << endl;
 //     for (int i = 0; i < myGraph.numVertices; i++) {
 //         for(int j = 0; j < myGraph.edges[i].size(); j++){
 //             // check if edge was already printed
@@ -189,7 +197,7 @@ void Graph::addTemporalVertices(std::vector<std::tuple<int, int, double, int>>* 
 //                 }
 //             }
 //             if(!alreadyPrinted)
-//                 std::cout << i << " --(" << myGraph.edges[i][j].weight << ")-- " << myGraph.edges[i][j].vertexId << std::endl;
+//                 cout << i << " --(" << myGraph.edges[i][j].weight << ")-- " << myGraph.edges[i][j].vertexId << endl;
 //         }
 //     }
 
